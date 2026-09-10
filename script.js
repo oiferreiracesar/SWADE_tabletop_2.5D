910a92f110baa9f55338ad2c71c10ab58c1409e1
oiferreiracesar <ceesarsantosjunior@gmail.com>
2026-09-10 19:44:14 -0300

    ok

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const levelHeight = 48;
 let blockHeight = 48; 
 const cutawayHeight = 12; 
 
-// VARIÁVEIS DE CENÁRIO DINÂMICO
+// VARIÁVEIS INICIAIS DO CENÁRIO
 let surfaceColor = '#1e293b'; 
 let undergroundColor = '#0a0705'; 
 let dirtColor = '#1e140f'; 
@@ -462,7 +462,6 @@ function renderCell(row, col, fIndex, isGhost, activeEraseMode = false, applyCut
     }
 
     if (isDirt) {
-        // ATUALIZADO: Cor da terra agora segue o bioma escolhido
         ctx.fillStyle = dirtColor; 
         ctx.beginPath();
         ctx.moveTo(pNorte.x, pNorte.y); ctx.lineTo(pLeste.x, pLeste.y); ctx.lineTo(pSul.x, pSul.y); ctx.lineTo(pOeste.x, pOeste.y);
@@ -707,26 +706,17 @@ document.getElementById('sliderAltura').addEventListener('input', (e) => {
     drawIsometricGrid();
 });
 
-// NOVO: Ouvinte de evento do Bioma
-document.getElementById('biomeSelect').addEventListener('change', (e) => {
-    const biome = e.target.value;
-    if (biome === 'default') {
-        surfaceColor = '#1e293b';     // Azul noturno
-        undergroundColor = '#0a0705'; // Vazio
-        dirtColor = '#1e140f';        // Terra marrom
-    } else if (biome === 'ocean') {
-        surfaceColor = '#0284c7';     // Água da superfície
-        undergroundColor = '#082f49'; // Profundezas do oceano
-        dirtColor = '#0c4a6e';        // Rocha molhada/azulada
-    } else if (biome === 'dungeon') {
-        surfaceColor = '#3f3f46';     // Cinza/neblina
-        undergroundColor = '#18181b'; // Masmorra negra
-        dirtColor = '#27272a';        // Pedra cinza escura
-    } else if (biome === 'hell') {
-        surfaceColor = '#7f1d1d';     // Céu de fogo
-        undergroundColor = '#450a0a'; // Abismo vulcânico
-        dirtColor = '#2e1012';        // Rocha vulcânica vermelha
-    }
+// NOVOS EVENTOS: Controle Direto de Cores do Mestre
+document.getElementById('colorSurface').addEventListener('input', (e) => {
+    surfaceColor = e.target.value;
+    drawIsometricGrid();
+});
+document.getElementById('colorUnderground').addEventListener('input', (e) => {
+    undergroundColor = e.target.value;
+    drawIsometricGrid();
+});
+document.getElementById('colorDirt').addEventListener('input', (e) => {
+    dirtColor = e.target.value;
     drawIsometricGrid();
 });
 
